0ea1dc60b150101e8787403e6c428e1a3f3a25f2
김규식 <gyusik6312@naver.com>
2026-09-16 00:45:05 +0900

    feat: 환경구축

---
+++ testfront/src/main/java/com/example/testfront/TestfrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.testfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestfrontApplication.class, args);
+	}
+
+}
---
+++ testfront/src/test/java/com/example/testfront/TestfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
